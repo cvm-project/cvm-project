96c46eae6535c781e01e6c4e971d051459fe6d92
Ingo Mueller <ingo.mueller@inf.ethz.ch>
2017-12-28 22:16:34 +0100

    Reformat commented out 'DEBUG_PRINT'.

--- cpp/src/utils/debug_print.h
+++ cpp/src/utils/debug_print.h
@@ -7,8 +7,10 @@
 #include <iostream>
 #include <string>
 
-//#define DEBUG_PRINT(x) do { std::clog<<"DEBUG " << #x << ": " << x <<
-// std::endl; } while (0)
+//#define DEBUG_PRINT(x)                                         \
+//    do {                                                       \
+//        std::clog << "DEBUG " << #x << ": " << x << std::endl; \
+//    } while (0)
 #define DEBUG_PRINT(x) \
     do {               \
     } while (0)
