f172aa57f60caff3406a1c8cd42e22b5e7d0b862
Ingo Mueller <ingo.mueller@inf.ethz.ch>
2018-06-01 18:54:12 +0200

    Include input ports in plan visualization.

--- backend/src/utils/printDAG.cpp
+++ backend/src/utils/printDAG.cpp
@@ -12,6 +12,8 @@
 
 #include <boost/algorithm/string/join.hpp>
 #include <boost/format.hpp>
+#include <boost/range/algorithm/transform.hpp>
+#include <boost/range/irange.hpp>
 
 #include <graphviz/gvc.h>
 
@@ -19,11 +21,26 @@
 #include "dag/DAGOperator.h"
 #include "utils/utils.h"
 
+std::string node_name(const DAGOperator *const op) {
+    return op->name() + "_" + std::to_string(op->id);
+}
+
+std::string head_port(const DAG::Flow &flow) {
+    if (flow.target->num_in_ports() > 1) {
+        return std::to_string(flow.target_port) + ":n";
+    }
+    return "n";
+}
+
+char *to_char_ptr(const std::string &s) {
+    return const_cast<char *>(s.c_str());
+}
+
 void buildDOT(const DAG *const dag, Agraph_t *g) {
     // Compute nodes from operators
     std::unordered_map<const DAGOperator *, Agnode_t *> nodes;
     for (const auto &op : dag->operators()) {
-        const std::string op_name = op->name() + "_" + std::to_string(op->id);
+        const std::string op_name = node_name(op);
 
         // Fields
         std::vector<std::string> fields;
@@ -52,11 +69,25 @@ void buildDOT(const DAG *const dag, Agraph_t *g) {
                            return "o_" + std::to_string(f.position);
                        });
 
+        // Input ports
+        const auto input_ports =
+                boost::irange(size_t{0}, op->num_in_ports()) |
+                boost::adaptors::transformed([](const auto &i) {
+                    const std::string s = std::to_string(i);
+                    return (boost::format("<%1%> %1%") % s).str();
+                });
+        const auto input_ports_label =
+                (boost::format("{%s} | ") % boost::join(input_ports, "| "))
+                        .str();
+        const auto input_port_label =
+                (op->num_in_ports() > 1) ? input_ports_label : std::string();
+
         // Compute node label
         const std::string node_label =
-                (boost::format(
-                         "%s\n\nfields: %s\nread: %s\nwrite: %s\ndead: %s\n") %
-                 op_name %                       //
+                (boost::format("{%s <header>%s | <fields> fields: "
+                               "%s&#92;nread: %s&#92;nwrite: %s&#92;ndead: "
+                               "%s&#92;n}") %
+                 input_port_label % op_name %    //
                  boost::join(fields, ", ") %     //
                  boost::join(read_set, ", ") %   //
                  boost::join(write_set, ", ") %  //
@@ -64,8 +95,9 @@ void buildDOT(const DAG *const dag, Agraph_t *g) {
                         .str();
 
         // Create node
-        Agnode_t *node = agnode(g, const_cast<char *>(node_label.c_str()), 1);
-        agsafeset(node, "shape", "polygon", "polygon");
+        Agnode_t *node = agnode(g, to_char_ptr(op_name), 1);
+        agsafeset(node, "shape", "record", "record");
+        agsafeset(node, "label", to_char_ptr(node_label), "");
         nodes.emplace(op, node);
     }
 
@@ -73,7 +105,9 @@ void buildDOT(const DAG *const dag, Agraph_t *g) {
     for (const auto &f : dag->flows()) {
         const auto &source_node = nodes.at(f.source);
         const auto &target_node = nodes.at(f.target);
-        agedge(g, source_node, target_node, "", 1);
+        Agedge_t *const edge = agedge(g, source_node, target_node, "", 1);
+
+        agsafeset(edge, "headport", to_char_ptr(head_port(f)), "");
     }
 }
 
