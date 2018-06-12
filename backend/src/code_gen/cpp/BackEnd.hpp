b31015129b29397a3b59293ec572e6e8601a12a2
Ingo Mueller <ingo.mueller@inf.ethz.ch>
2018-06-12 13:09:13 +0200

    Include what you use in cpp/BackEnd.cpp.

--- backend/src/code_gen/cpp/BackEnd.hpp
+++ backend/src/code_gen/cpp/BackEnd.hpp
@@ -2,13 +2,14 @@
 #define CODE_GEN_CPP_BACKEND_HPP
 
 #include "code_gen/common/BackEnd.hpp"
+#include "dag/DAG.h"
 
 namespace code_gen {
 namespace cpp {
 
 class BackEnd : public common::BackEnd {
 public:
-    void GenerateCode(DAG* dag) final;
+    void GenerateCode(DAG *dag) final;
     void Compile(uint64_t counter) final;
 };
 
