5b310d4723a8eb2574be17ccf32be82fac7f816b
Ingo Mueller <ingo.mueller@inf.ethz.ch>
2018-06-27 18:42:12 +0200

    Header guards for compiler/generate_executable.hpp.

--- backend/src/compiler/generate_executable.hpp
+++ backend/src/compiler/generate_executable.hpp
@@ -1,3 +1,8 @@
+#ifndef COMPILER_GENERATE_EXECUTABLE_HPP
+#define COMPILER_GENERATE_EXECUTABLE_HPP
+
 extern "C" {
 #include "generate_executable.h"
 }
+
+#endif  // COMPILER_GENERATE_EXECUTABLE_HPP
