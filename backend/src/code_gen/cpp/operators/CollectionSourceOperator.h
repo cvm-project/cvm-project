b040721aef8a4b556e6e43b7cb698c74f94d5faa
Ingo Mueller <ingo.mueller@inf.ethz.ch>
2018-07-08 15:20:59 +0200

    Make cpp CollectionOperator adhere to naming conventions.

--- backend/src/code_gen/cpp/operators/CollectionSourceOperator.h
+++ backend/src/code_gen/cpp/operators/CollectionSourceOperator.h
@@ -9,26 +9,26 @@
 
 #include "Operator.h"
 
-template <class Tuple, bool add_index, class Upstream>
+template <class Tuple, bool kAddIndex, class Upstream>
 class CollectionSourceOperator : public Operator {
 public:
-    CollectionSourceOperator(Upstream *const upstream) : upstream(upstream) {}
+    CollectionSourceOperator(Upstream *const upstream) : upstream_(upstream) {}
 
     INLINE void open() {
-        upstream->open();
-        auto input_tuple = upstream->next().value;
-        upstream->close();
+        upstream_->open();
+        auto input_tuple = upstream_->next().value;
+        upstream_->close();
         // XXX: This is currently a memory leak! We need a clear way to manage
         //      memory that releases allocated memory as soon as it is not used
-        values = input_tuple.v0.data;
-        size = input_tuple.v0.shape[0];
-        index = 0;
+        values_ = input_tuple.v0.data;
+        size_ = input_tuple.v0.shape[0];
+        index_ = 0;
     }
 
     INLINE Optional<Tuple> next() {
-        if (index < size) {
-            Tuple r = build_result();
-            index++;
+        if (index_ < size_) {
+            Tuple r = BuildResult();
+            index_++;
             return r;
         }
         return {};
@@ -37,30 +37,30 @@ public:
     INLINE void close() {}
 
 private:
-    Upstream *const upstream;
-    size_t size;
-    size_t index;
-    void *values;
+    Upstream *const upstream_;
+    size_t size_;
+    size_t index_;
+    void *values_;
 
-    INLINE Tuple build_result() {
+    INLINE Tuple BuildResult() {
         Tuple res;
         char *resp = (char *)&res;
-        if (add_index) {
+        if (kAddIndex) {
             size_t tuple_size = sizeof(Tuple) - sizeof(size_t);
-            *((size_t *)resp) = index;
-            memcpy(resp + sizeof(size_t), (char *)values + tuple_size * index,
+            *((size_t *)resp) = index_;
+            memcpy(resp + sizeof(size_t), (char *)values_ + tuple_size * index_,
                    tuple_size);
         } else {
-            *((Tuple *)resp) = ((Tuple *)values)[index];
+            *((Tuple *)resp) = ((Tuple *)values_)[index_];
         }
         return res;
     }
 };
 
-template <class Tuple, bool add_index, class Upstream>
-CollectionSourceOperator<Tuple, add_index, Upstream>
+template <class Tuple, bool kAddIndex, class Upstream>
+CollectionSourceOperator<Tuple, kAddIndex, Upstream>
 makeCollectionSourceOperator(Upstream *const upstream) {
-    return CollectionSourceOperator<Tuple, add_index, Upstream>(upstream);
+    return CollectionSourceOperator<Tuple, kAddIndex, Upstream>(upstream);
 }
 
 #endif  // CPP_COLLECTIONSOURCEOPERATOR_H
