5c8dbd4f60f55a8e8562397ea14ec339eeaf83b3
Ingo Mueller <ingo.mueller@inf.ethz.ch>
2018-07-08 15:26:29 +0200

    Clean up cpp MaterializeOperator.
    Make adhere to naming conventions and open upstream in
    `MaterializeUpstream`, where the matching `close` call is.

--- backend/src/code_gen/cpp/operators/MaterializeRowVectorOperator.h
+++ backend/src/code_gen/cpp/operators/MaterializeRowVectorOperator.h
@@ -14,41 +14,48 @@ auto constexpr max(T x, U y) ->
 template <class OuterTuple, class InnerTuple, class Upstream>
 class MaterializeRowVectorOperator : public Operator {
 public:
-    MaterializeRowVectorOperator(Upstream *upstream)
-        : upstream(upstream), result_ptr(nullptr), result_size(0) {}
+    MaterializeRowVectorOperator(Upstream *const upstream)
+        : upstream_(upstream), result_ptr_(nullptr), result_size_(0) {}
 
-    INLINE void open() { upstream->open(); }
+    INLINE void open() {}
 
     INLINE Optional<OuterTuple> next() {
-        if (result_ptr == nullptr) materialize_upstream();
-        return OuterTuple{result_ptr, result_size};
+        if (result_ptr_ == nullptr) MaterializeUpstream();
+        return OuterTuple{result_ptr_, result_size_};
     }
 
     INLINE void close() {}
 
 private:
-    void materialize_upstream() {
+    void MaterializeUpstream() {
+        upstream_->open();
+
         size_t allocated_size = 1;
-        result_ptr = reinterpret_cast<InnerTuple *>(
+        result_ptr_ = reinterpret_cast<InnerTuple *>(
                 malloc(sizeof(InnerTuple) * allocated_size));
-        while (auto tuple = upstream->next()) {
-            if (allocated_size <= result_size) {
+        while (auto tuple = upstream_->next()) {
+            if (allocated_size <= result_size_) {
                 allocated_size *= 2;
-                result_ptr = reinterpret_cast<InnerTuple *>(realloc(
-                        result_ptr, sizeof(InnerTuple) * allocated_size));
-                if (result_ptr == nullptr) throw "out of memory";
+                result_ptr_ = reinterpret_cast<InnerTuple *>(realloc(
+                        result_ptr_, sizeof(InnerTuple) * allocated_size));
+                if (result_ptr_ == nullptr)
+                    throw std::runtime_error(
+                            "Could not materialize: out of memory.");
             }
-            result_ptr[result_size] = tuple.value;
-            result_size++;
+            result_ptr_[result_size_] = tuple.value;
+            result_size_++;
         }
-        result_ptr = reinterpret_cast<InnerTuple *>(realloc(
-                result_ptr, sizeof(InnerTuple) * max(size_t(1), result_size)));
-        upstream->close();
+
+        result_ptr_ = reinterpret_cast<InnerTuple *>(
+                realloc(result_ptr_,
+                        sizeof(InnerTuple) * max(size_t(1), result_size_)));
+
+        upstream_->close();
     }
 
-    Upstream *const upstream;
-    InnerTuple *result_ptr;
-    size_t result_size;
+    Upstream *const upstream_;
+    InnerTuple *result_ptr_;
+    size_t result_size_;
 };
 
 template <class OuterTuple, class InnerTuple, class Upstream>
