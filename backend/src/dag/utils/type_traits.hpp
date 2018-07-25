e52cd6a8194b63519a4c6033a0b88bf45d6206da
Ingo Mueller <ingo.mueller@inf.ethz.ch>
2018-07-25 15:55:53 +0200

    Add helper to detect runtime type of DAGOperators.

---
+++ backend/src/dag/utils/type_traits.hpp
@@ -0,0 +1,54 @@
+#ifndef DAG_UTILS_TYPE_TRAITS_HPP
+#define DAG_UTILS_TYPE_TRAITS_HPP
+
+#include <type_traits>
+
+#include <boost/mpl/contains.hpp>
+#include <boost/mpl/list.hpp>
+#include <boost/mpl/transform_view.hpp>
+
+#include "utils/visitor.hpp"
+
+class DAG;
+class DAGOperator;
+
+namespace dag {
+namespace utils {
+
+namespace impl {
+
+template <typename... OperatorTypes>
+class IsInstanceOfVisitor;
+
+}  // namespace impl
+
+template <typename... OperatorTypes>
+bool IsInstanceOf(const DAGOperator *const op) {
+    return impl::IsInstanceOfVisitor<OperatorTypes...>().Visit(op);
+}
+
+namespace impl {
+
+template <typename... OperatorTypes>
+class IsInstanceOfVisitor
+    : public Visitor<IsInstanceOfVisitor<OperatorTypes...>, const DAGOperator,
+                     typename boost::mpl::list<OperatorTypes...>::type, bool> {
+public:
+    using OperatorTypeList = typename boost::mpl::transform_view<
+            boost::mpl::list<OperatorTypes...>,
+            std::remove_cv<boost::mpl::placeholders::_>>::type;
+    template <class OperatorType,
+              typename = std::enable_if_t<boost::mpl::contains<
+                      OperatorTypeList, std::remove_cv_t<OperatorType>>::value>>
+    bool operator()(OperatorType *const /*op*/) {
+        return true;
+    }
+    bool operator()(DAGOperator *const /*op*/) { return false; }
+};
+
+}  // namespace impl
+
+}  // namespace utils
+}  // namespace dag
+
+#endif  // DAG_UTILS_TYPE_TRAITS_HPP
