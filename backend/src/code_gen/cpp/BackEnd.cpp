e565196a5bc95c5f7273798198f83adccf7e2546
Ingo Mueller <ingo.mueller@inf.ethz.ch>
2018-07-27 09:38:12 +0200

    Make variables of cpp/BackEnd.cpp adhere to naming conventions.

--- backend/src/code_gen/cpp/BackEnd.cpp
+++ backend/src/code_gen/cpp/BackEnd.cpp
@@ -18,19 +18,19 @@ namespace code_gen {
 namespace cpp {
 
 void BackEnd::GenerateCode(DAG *const dag) {
-    const std::string genDir = get_lib_path() + "/backend/gen/";
-    exec(("bash -c 'rm -r -f " + genDir + "/*'").c_str());
+    const std::string gen_dir = get_lib_path() + "/backend/gen/";
+    exec(("bash -c 'rm -r -f " + gen_dir + "/*'").c_str());
 
     // Create output directory
-    const int dirErr = system(("mkdir -p " + genDir).c_str());
-    if (0 != dirErr) {
+    const int dir_err = system(("mkdir -p " + gen_dir).c_str());
+    if (0 != dir_err) {
         std::cerr << ("Error creating gen directory!") << std::endl;
         std::exit(1);
     }
 
     // Setup visitor and run it
-    std::string llvmCodePath = genDir + "llvm_funcs.ll";
-    std::ofstream llvmCode(llvmCodePath);
+    std::string llvm_code_path = gen_dir + "llvm_funcs.ll";
+    std::ofstream llvm_code(llvm_code_path);
 
     std::stringstream declarations;
     std::stringstream definitions;
@@ -39,45 +39,44 @@ void BackEnd::GenerateCode(DAG *const dag) {
     std::set<std::string> includes;
     Context::TupleTypeRegistry tuple_type_descs;
 
-    Context context(&declarations, &definitions, &llvmCode, &unique_counters,
+    Context context(&declarations, &definitions, &llvm_code, &unique_counters,
                     &includes, &tuple_type_descs);
 
     auto execute_values = GenerateExecutePipelines(&context, dag);
     assert(execute_values == "execute_pipelines");
 
     // Main executable file: declarations
-    std::ofstream mainSourceFile(genDir + "execute.cpp");
+    std::ofstream source_file(gen_dir + "execute.cpp");
 
-    mainSourceFile << "/**\n"
-                      " * Auto-generated execution plan\n"
-                      " */\n";
+    source_file << "/**\n"
+                   " * Auto-generated execution plan\n"
+                   " */\n";
 
     includes.emplace("\"runtime/free.hpp\"");
 
     for (const auto &incl : context.includes()) {
-        mainSourceFile << "#include " << incl << std::endl;
+        source_file << "#include " << incl << std::endl;
     }
 
-    mainSourceFile << "using namespace runtime::values;" << std::endl;
+    source_file << "using namespace runtime::values;" << std::endl;
 
-    mainSourceFile << declarations.str();
-    mainSourceFile << definitions.str();
+    source_file << declarations.str();
+    source_file << definitions.str();
 
     // Main executable file: exported execute function
-    mainSourceFile
-            << "extern \"C\" { const char* execute("
-               "        const char *const inputs_str) {"
-               "    const auto inputs = ConvertFromJsonString(inputs_str);"
-               "    const auto ret = execute_pipelines(inputs);"
-               "    const auto ret_str = ConvertToJsonString(ret);"
-               "    const auto ret_ptr = reinterpret_cast<char *>("
-               "            malloc(ret_str.size() + 1));"
-               "    strcpy(ret_ptr, ret_str.c_str());"
-               "    return ret_ptr;"
-               "} }";
+    source_file << "extern \"C\" { const char* execute("
+                   "        const char *const inputs_str) {"
+                   "    const auto inputs = ConvertFromJsonString(inputs_str);"
+                   "    const auto ret = execute_pipelines(inputs);"
+                   "    const auto ret_str = ConvertToJsonString(ret);"
+                   "    const auto ret_ptr = reinterpret_cast<char *>("
+                   "            malloc(ret_str.size() + 1));"
+                   "    strcpy(ret_ptr, ret_str.c_str());"
+                   "    return ret_ptr;"
+                   "} }";
 
     // Main executable file: free
-    mainSourceFile <<  //
+    source_file <<  //
             "extern \"C\" {"
             "    void free_result(const char* const s) {"
             "        runtime::FreeValues(s);"
@@ -87,9 +86,9 @@ void BackEnd::GenerateCode(DAG *const dag) {
             "}";
 
     // Header file
-    std::ofstream headerFile(genDir + "execute.h");
+    std::ofstream header_file(gen_dir + "execute.h");
 
-    headerFile <<  //
+    header_file <<  //
             "const char* execute(const char* inputs_str);\n"
             "void free_result(const char*);";
 }
