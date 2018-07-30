af3e8a321d1705a3dabd5a86a56e045114d0ce58
Ingo Mueller <ingo.mueller@inf.ethz.ch>
2018-07-30 11:31:57 +0200

    Parallelize joins with sequential join of partitions.

--- backend/src/optimize/parallelize.cpp
+++ backend/src/optimize/parallelize.cpp
@@ -93,7 +93,8 @@ void Parallelize::Run(DAG *const dag) const {
         auto const inner_dag = dag->inner_dag(op);
         do {
             if (dag->out_degree(op) != 1) break;
-            if (IsInstanceOf<DAGFilter, DAGMap>(dag->successor(op))) {
+            if (IsInstanceOf<DAGFilter, DAGMap, DAGPartition>(
+                        dag->successor(op))) {
                 const auto other_op = dag->successor(op);
                 const auto next_op = dag->successor(other_op);
 
@@ -203,6 +204,129 @@ void Parallelize::Run(DAG *const dag) const {
                 next_inner_dag->AddFlow(scan_op1, scan_op2);
                 next_inner_dag->AddFlow(scan_op2, red_op);
                 next_inner_dag->set_output(red_op);
+
+                // Remember the next parallel map such that it can be extended
+                parallelize_operators.emplace(next_pop);
+
+                break;
+            }
+            if (IsInstanceOf<DAGJoin>(dag->successor(op))) {
+                auto const join_op = dag->successor(op);
+
+                auto const this_in_flow = dag->out_flow(op);
+                auto const this_in_port = this_in_flow.target.port;
+                auto const other_in_port = this_in_port == 0 ? 1 : 0;
+                auto const other_in_flow = dag->in_flow(join_op, other_in_port);
+                assert(this_in_port + other_in_port == 1);
+
+                // (1) Prepare this side's input of the join
+
+                // Partition this side's input by join key
+                auto const this_part_op = new DAGPartition();
+                inner_dag->AddOperator(this_part_op);
+
+                // End this side's parallel map of the input
+                inner_dag->AddFlow(inner_dag->output().op, this_part_op);
+                inner_dag->set_output(this_part_op);
+
+                // GroupBy operator after this side's parallel map
+                auto const this_grouping_op = new DAGGroupBy();
+                dag->AddOperator(this_grouping_op);
+
+                // (2) Prepare other side's input of the join
+
+                // Partition other side's input by join key
+                auto const other_part_op = new DAGPartition();
+                dag->AddOperator(other_part_op);
+
+                // GroupBy operator after other side's partitioning
+                auto const other_grouping_op = new DAGGroupBy();
+                dag->AddOperator(other_grouping_op);
+
+                // (3) Outer level join of partitions
+
+                // Reconnect outer level before join
+                dag->RemoveFlow(this_in_flow);
+                dag->RemoveFlow(other_in_flow);
+
+                dag->AddFlow(op, this_grouping_op);
+                dag->AddFlow(this_grouping_op, join_op, this_in_port);
+
+                dag->AddFlow(other_in_flow.source.op, other_in_flow.source.port,
+                             other_part_op);
+                dag->AddFlow(other_part_op, other_grouping_op);
+                dag->AddFlow(other_grouping_op, join_op, other_in_port);
+
+                // Project away the partition IDs
+                auto const proj_op = new DAGProjection();
+                dag->AddOperator(proj_op);
+                proj_op->positions = {1, 2};
+
+                // Next parallel map (for the actual join)
+                auto const next_pop = new DAGParallelMap();
+                dag->AddOperator(next_pop);
+                dag->set_inner_dag(next_pop, new DAG());
+                auto const next_inner_dag = dag->inner_dag(next_pop);
+
+                // Reconnect outer level after the join
+                auto const out_flow = dag->out_flow(join_op);
+                dag->RemoveFlow(out_flow);
+
+                dag->AddFlow(join_op, proj_op);
+                dag->AddFlow(proj_op, next_pop);
+                dag->AddFlow(next_pop, out_flow.target.op,
+                             out_flow.target.port);
+
+                // (4) Fill the next parallel map with the actual join
+
+                // Unzip the two inputs (of each partition)
+                auto const left_param_op = new DAGParameterLookup();
+                next_inner_dag->AddOperator(left_param_op);
+
+                auto const right_param_op = new DAGParameterLookup();
+                next_inner_dag->AddOperator(right_param_op);
+
+                auto const left_proj_op = new DAGProjection();
+                next_inner_dag->AddOperator(left_proj_op);
+                left_proj_op->positions = {0};
+
+                auto const right_proj_op = new DAGProjection();
+                next_inner_dag->AddOperator(right_proj_op);
+                right_proj_op->positions = {1};
+
+                // Scan operators for (1) partitioning and (2) groupby
+                auto const left_scan_op1 = new DAGCollection();
+                next_inner_dag->AddOperator(left_scan_op1);
+
+                auto const left_scan_op2 = new DAGCollection();
+                next_inner_dag->AddOperator(left_scan_op2);
+
+                auto const right_scan_op1 = new DAGCollection();
+                next_inner_dag->AddOperator(right_scan_op1);
+
+                auto const right_scan_op2 = new DAGCollection();
+                next_inner_dag->AddOperator(right_scan_op2);
+
+                // Use the original join operator to do the actual join
+                auto const inner_join_op = new DAGJoin();
+                next_inner_dag->AddOperator(inner_join_op);
+
+                // Connect new operators in next inner DAG
+                assert(next_inner_dag->in_degree() == 0);
+                next_inner_dag->add_input(0, left_param_op);
+                next_inner_dag->add_input(0, right_param_op);
+
+                next_inner_dag->AddFlow(left_param_op, left_proj_op);
+                next_inner_dag->AddFlow(left_proj_op, left_scan_op1);
+                next_inner_dag->AddFlow(left_scan_op1, left_scan_op2);
+                next_inner_dag->AddFlow(left_scan_op2, inner_join_op, 0);
+
+                next_inner_dag->AddFlow(right_param_op, right_proj_op);
+                next_inner_dag->AddFlow(right_proj_op, right_scan_op1);
+                next_inner_dag->AddFlow(right_scan_op1, right_scan_op2);
+                next_inner_dag->AddFlow(right_scan_op2, inner_join_op, 1);
+
+                next_inner_dag->set_output(inner_join_op);
 
                 // Remember the next parallel map such that it can be extended
                 parallelize_operators.emplace(next_pop);
