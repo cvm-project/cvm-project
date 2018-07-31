2a5830b1412ef86b3c483965f078bc0acd9d2665
Ingo Mueller <ingo.mueller@inf.ethz.ch>
2018-07-31 16:47:46 +0200

    Add Python-level tests for parallel execution.

---
+++ python/jitq/tests/test_cpp_backend_parallel.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+
+from jitq.tests.helpers import run_tests, load_and_patch_common_tests, \
+    load_tests_common_patched
+from jitq.jitq_context import JitqContext
+
+
+def set_up(self):
+    self.context = JitqContext(conf={
+        "codegen": {"backend": "cpp"},
+        "verbose": True,
+        "optimizer": {"optimizations": {"parallelize": True}},
+    })
+
+
+NAME_SUFFIX = "CppParallel"
+FUNCTIONS = {"setUp": set_up}
+
+
+def load_tests(loader, _standard_tests, _pattern):
+    return load_tests_common_patched(
+        loader, NAME_SUFFIX, FUNCTIONS)
+
+
+load_and_patch_common_tests(FUNCTIONS)
+if __name__ == '__main__':
+    run_tests()
