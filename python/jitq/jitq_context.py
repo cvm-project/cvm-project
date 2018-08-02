7f97de12c395b92f2e04187b81226b61e1d9af05
Ingo Mueller <ingo.mueller@inf.ethz.ch>
2018-08-02 11:24:31 +0200

    Load configuration from environment (if present).

--- python/jitq/jitq_context.py
+++ python/jitq/jitq_context.py
@@ -1,15 +1,30 @@
+import json
+import os
+
 import jsonmerge
 
 from jitq.rdd import CollectionSource, GeneratorSource, \
     Range, ParameterLookup
+from jitq.utils import get_project_path
 
 
 class JitqContext:
     default_conf = {}
 
     def __init__(self, **kwargs):
-        conf = kwargs.get('conf', {})
-        self.conf = jsonmerge.merge(JitqContext.default_conf, conf)
+        local_conf = kwargs.get('conf', {})
+
+        env_conf = {}
+        env_conf_path = get_project_path() + "/jitq_config.json"
+        if os.path.isfile(env_conf_path):
+            with open(env_conf_path) as env_conf_file:
+                env_conf = json.load(env_conf_file)
+
+        conf = JitqContext.default_conf
+        conf = jsonmerge.merge(conf, env_conf)
+        conf = jsonmerge.merge(conf, local_conf)
+        self.conf = conf
+
         self.serialization_cache = {}
         self.executor_cache = {}
 
