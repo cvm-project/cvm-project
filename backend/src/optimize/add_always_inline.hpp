27c60f8940ea37bf19f0afb4b59f0b46afe1428d
Ingo Mueller <ingo.mueller@inf.ethz.ch>
2019-03-10 01:33:25 +0100

    STYLE: Clean up add_always_inline.hpp.

--- backend/src/optimize/add_always_inline.hpp
+++ backend/src/optimize/add_always_inline.hpp
@@ -1,5 +1,5 @@
-#ifndef OPTIMIZA_ADD_ALWAYS_INLINE_HPP
-#define OPTIMIZA_ADD_ALWAYS_INLINE_HPP
+#ifndef OPTIMIZE_ADD_ALWAYS_INLINE_HPP
+#define OPTIMIZE_ADD_ALWAYS_INLINE_HPP
 
 #include "dag_transformation.hpp"
 
@@ -10,5 +10,7 @@ public:
     void Run(DAG *dag) const override;
     std::string name() const override { return "add_always_inline"; }
 };
+
 }  // namespace optimize
-#endif  // OPTIMIZA_ADD_ALWAYS_INLINE_HPP
+
+#endif  // OPTIMIZE_ADD_ALWAYS_INLINE_HPP
