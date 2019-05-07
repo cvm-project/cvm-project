2f8214b7416a0a573723115590919a367f0e1932
Ingo Mueller <ingo.mueller@inf.ethz.ch>
2019-05-07 19:08:38 +0200

    Use utils::Registry in execute_plan.cpp.
    This solves #59.

--- backend/src/runtime/execute_plan.cpp
+++ backend/src/runtime/execute_plan.cpp
@@ -3,31 +3,25 @@
 #include <cstring>
 #include <vector>
 
+#include "utils/registry.hpp"
 #include "values/json_parsing.hpp"
 
 namespace runtime {
-namespace impl {
 
-using PlanRegistry = std::vector<Plan>;
-
-static PlanRegistry *plan_registry() {
-    static PlanRegistry registry;
-    return &registry;
-}
-
-}  // namespace impl
+using PlanRegistry = utils::Registry<Plan, size_t>;
 
 std::string ExecutePlan(const size_t plan_id, const std::string &inputs_str) {
     const auto inputs =
             runtime::values::ConvertFromJsonString(inputs_str.c_str());
-    const auto plan = impl::plan_registry()->at(plan_id);
+    const auto plan = *(PlanRegistry::at(plan_id));
     const auto ret = plan(inputs);
     return runtime::values::ConvertToJsonString(ret);
 }
 
 size_t RegisterPlan(const Plan &plan) {
-    impl::plan_registry()->emplace_back(plan);
-    return impl::plan_registry()->size() - 1;
+    auto const plan_id = PlanRegistry::num_objects();
+    PlanRegistry::Register(plan_id, std::make_unique<Plan>(plan));
+    return plan_id;
 }
 
 }  // namespace runtime
