014ccbd9ff0950b324e56a73b670f251929ba2e5
Ingo Mueller <ingo.mueller@inf.ethz.ch>
2019-08-06 17:03:43 +0200

    Remove unused functions and unintentially checked in binary data.

--- backend/src/utils/tuple_to_string.h
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-#include <string>
-#include <tuple>
-
-template <typename... T>
-std::string to_string(const std::tuple<T...> &tup);
-
-template <class T>
-std::string to_string(const T v);
