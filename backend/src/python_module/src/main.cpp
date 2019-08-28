00f26047cc9b99999994be4f1dc367ea67ae2aeb
Ingo Mueller <ingo.mueller@inf.ethz.ch>
2019-08-28 10:33:10 +0200

    Release Python's GIL while inside native module.

--- backend/src/python_module/src/main.cpp
+++ backend/src/python_module/src/main.cpp
@@ -1,8 +1,11 @@
+#include <pybind11/embed.h>
 #include <pybind11/pybind11.h>
 
 #include "generate/generate_executable.hpp"
 #include "runtime/execute_plan.hpp"
 #include "runtime/memory/values.hpp"
+
+namespace py = pybind11;
 
 // NOLINTNEXTLINE  This macro expands to things clang-tidy does not like,
 // NOLINTNEXTLINE  but we do not have control over it.
@@ -17,24 +20,29 @@ PYBIND11_MODULE(jitq_backend, m) {
            :toctree: _generate
     )pbdoc";
 
-    m.def("GenerateExecutable", &GenerateExecutable, R"pbdoc(
+    m.def("GenerateExecutable", &GenerateExecutable,
+          py::call_guard<py::gil_scoped_release>(),  //
+          R"pbdoc(
         Generate executable plan
     )pbdoc");
 
     m.def("ExecutePlan",
           static_cast<std::string (*)(size_t, const std::string&)>(
                   runtime::ExecutePlan),
+          py::call_guard<py::gil_scoped_release>(),  //
           R"pbdoc(
         Execute previously generated plan
     )pbdoc");
 
     m.def("DumpDag", runtime::DumpDag,
+          py::call_guard<py::gil_scoped_release>(),  //
           R"pbdoc(
         Dump previously generated plan as JSON string
     )pbdoc");
 
     m.def("FreeResult",
           static_cast<void (*)(const std::string&)>(runtime::memory::Decrement),
+          py::call_guard<py::gil_scoped_release>(),  //
           R"pbdoc(
         Free memory of result
     )pbdoc");
