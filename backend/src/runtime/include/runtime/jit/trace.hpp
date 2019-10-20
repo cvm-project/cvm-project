d451f9a8679d7e12266d12f7a99fdd506b7bda9b
Ingo Mueller <ingo.mueller@inf.ethz.ch>
2019-10-20 17:29:51 +0200

    Add simple tracing function to backend.

---
+++ backend/src/runtime/include/runtime/jit/trace.hpp
@@ -0,0 +1,8 @@
+#ifndef RUNTIME_JIT_TRACE_HPP
+#define RUNTIME_JIT_TRACE_HPP
+
+#include <string>
+
+void Trace(const std::string &message);
+
+#endif  // RUNTIME_JIT_TRACE_HPP
---
+++ backend/src/runtime/src/trace.cpp
@@ -0,0 +1,15 @@
+#include <chrono>
+#include <iostream>
+#include <mutex>
+#include <string>
+#include <thread>
+
+void Trace(const std::string &message) {
+    static std::mutex trace_mutex;
+    std::lock_guard<std::mutex> l(trace_mutex);
+    std::cerr << std::chrono::duration_cast<std::chrono::microseconds>(
+                         std::chrono::steady_clock::now().time_since_epoch())
+                         .count()
+              << ": [" << std::this_thread::get_id() << "] " << message
+              << std::endl;
+}
