2732fd8a0a08bba83e6ee2db41a05c57f67965d0
Ingo Mueller <ingo.mueller@inf.ethz.ch>
2020-05-28 12:10:22 +0200

    Implement decorator that allows to skip individual targets on a test.

--- python/jitq/tests/helpers.py
+++ python/jitq/tests/helpers.py
@@ -1,5 +1,8 @@
 import argparse
+import functools
 import json
+
+import pytest
 
 
 def str2bool(value):
@@ -14,3 +17,23 @@ def str2bool(value):
 
 def format_json(json_string):
     return json.dumps(json.loads(json_string), sort_keys=True, indent=4)
+
+
+def only_targets(targets=None, skip=None):
+    targets = targets or []
+    skip_targets = skip or []
+
+    def decorator(func):
+        @functools.wraps(func)
+        def wrapper(*args, **kwargs):
+            jitq_context = kwargs['jitq_context']
+            target = jitq_context.conf \
+                .get('optimizer', {}) \
+                .get('target', None)
+            if (target not in targets) or \
+                    (not targets and target in skip_targets):
+                pytest.skip('Target "{}" currently not supported'
+                            .format(target))
+            func(*args, **kwargs)
+        return wrapper
+    return decorator
